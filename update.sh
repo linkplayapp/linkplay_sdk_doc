b3538a086c70f0cfb2bb98f0917c3f586cc41849
shuai.zhao <shuai.zhao@linkplay.com>
2020-05-27 17:42:33 +0800

    Document updated: 2020-05-27 17:42:33

---
+++ update.sh
@@ -0,0 +1,19 @@
+rm -rf /tmp/_book
+cp -r _book /tmp
+
+git add .
+stash_result=`git stash`
+
+git checkout gh-pages
+rm -rf ./*
+mv /tmp/_book/* .
+
+git add .
+time_stamp=`date +"%Y-%m-%d %H:%M:%S"`
+git commit -m "Document updated: $time_stamp"
+git push origin gh-pages
+
+git checkout master
+if [[ "$stash_result" != "No local changes to save" ]]; then
+    git stash pop
+fi
